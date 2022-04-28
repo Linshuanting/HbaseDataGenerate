fe806d65ae4b02d7be6075599edfafc05921bfbf
as6213 <tim891209@gmail.com>
2022-04-28 21:42:15 +0800

    Add getData code comments

--- src/main/java/com/data/DataGet.java
+++ src/main/java/com/data/DataGet.java
@@ -12,6 +12,30 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 
 import java.io.IOException;
 
+ /**
+ * 此程式為運行 hbase table3, table4, table5 的程式
+ * table3 格式為row_key: xxx_phonenum
+ *   columnFamily: All_of_the_time
+ *   columnQualifier: 2020-04-30T00:00:00(time)
+ *   value: placeCode
+ * table4 格式為
+ *   row_key: rk: xxx_placecode_time
+ *   columnFamily: People
+ *   columnQualifier: phonenum
+ *   value: x
+ * table5 格式為
+ *   row_key: rk: xxx_placecode_time
+ *   columnFamily: All_position_time
+ *   columnQualifier: 2020-04-30T00:00:00(time)
+ *   value: phonenum
+ * 1. 透過 table3, table4，可以得到一筆潛在確診者清單
+ * 2. 透過 table3, table5，也可以得到一筆潛在確診者清單
+ * 而此程式最終會輸出：運行此兩者所需要的時間，來比較資料庫設計運行差異
+ * 運行此程式最後得到的潛在確診者清單，會存入txt中，而檔名需要自己設定*
+  *
+  * @author Lin Hsuan-Ting
+ */
+
 public class DataGet {
 
     // 新增需要連接到Hbase的設定
@@ -28,9 +52,6 @@ public class DataGet {
 
     }
 
-    /*
-
-     */
     public static void main(String[] args) throws IOException {
         
         String[][] data01, data02;
@@ -42,6 +63,8 @@ public class DataGet {
         // 開始 table34 的讀取資料，並啟動計時器
         // 需要選擇一個確診的人，並匡列出此人行動的時間
         // 得到的 data 即為可能確診接觸人
+        // 時間需要依照該格式：2020-04-30T00:00:00
+        // minTime, maxTime 為開始的時間以及結束的時間
         timer.startTimer();
         data01 = getDataByTable34("0912545411",
                             "2020-04-1T00:00:00",
@@ -59,11 +82,9 @@ public class DataGet {
         timer.stopTimer();
         System.out.println("data01 run time: " + timer.getRunTime());
 
-        fileOperator.writeTotxt("data01", data02);
+        fileOperator.writeTotxt("data02", data02);
 
     }
-
-
 
 
     // 取得 table3 & table4 合作的資料
